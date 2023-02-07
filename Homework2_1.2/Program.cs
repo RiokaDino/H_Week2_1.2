895861d4921bba486f8e8470d0fd7b695c870edb
RiokaDino <110732907+RiokaDino@users.noreply.github.com>
2023-02-07 23:44:21 +0700

    Week2_1..2

---
+++ Homework2_1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+      Console.Write("Input number : "); 
+      double x = double.Parse(Console.ReadLine());
+      Console.WriteLine(Function(x));  
+    }
+    static double Function(double x)
+    {
+      if(Math.Pow(x,2) <= 4)
+      {
+        return 2;
+      }
+      else 
+      {
+        return 1+Function(x-(4*Math.Abs(x))/x);
+      }
+    }
+}
